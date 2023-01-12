a7b39e089a39d91797d23d02296bbc9ef41ea37d
SamaneSharifiMonfared <samane.sharify@gmail.com>
2023-01-12 11:23:19 +0000

    Anonymous Functions

--- SecoundApp-Node-JavaScript/app.js
+++ SecoundApp-Node-JavaScript/app.js
@@ -2,11 +2,10 @@ var http=require('http');
 var module1=require('./module1');
 var module2=require('./module2');
 
-function onRequest(request,response){
+
+http.createServer(function (request,response){
     response.writeHead(200,{'Content-Type':'text/plain'});
     response.write(module2.myString);
     module2.myFunction();
     response.end();
-}
-
-http.createServer(onRequest).listen(8000);
+}).listen(8000);
